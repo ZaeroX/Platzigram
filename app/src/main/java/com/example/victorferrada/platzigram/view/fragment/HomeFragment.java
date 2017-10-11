4645324dc83d9183aec6e893e254a239ab3ff36e
Victor Ferrada <victor.ferrada@cadem.cl>
2017-10-11 18:08:14 -0300

    termino del curso
    acabamos el curso de android apra dar paso a  el curso definitivo

--- app/src/main/java/com/example/victorferrada/platzigram/view/fragment/HomeFragment.java
+++ app/src/main/java/com/example/victorferrada/platzigram/view/fragment/HomeFragment.java
@@ -3,11 +3,20 @@ package com.example.victorferrada.platzigram.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.victorferrada.platzigram.R;
+import com.example.victorferrada.platzigram.adapter.PictureAdapterRecyclerView;
+import com.example.victorferrada.platzigram.model.Picture;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +33,35 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        showtoolbar(getResources().getString(R.string.tab_home), false, view);
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buidPictures(),R.layout.cardview_picture, getActivity());
+
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+    public ArrayList<Picture> buidPictures(){
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg","Victor Ferrada","4 dias","3 Me Gusta"));
+        pictures.add(new Picture("http://www.enjoyart.com/library/landscapes/italianlandscapesseascapes/large/TC-HBLakeGarda-jpg.jpg","Victor Galaz","3 dias","10 Me Gusta"));
+        pictures.add(new Picture("http://www.educationquizzes.com/library/KS3-Geography/river-1-1.jpg","Manuel Andres","2 dias","3 Me Gusta"));
+        return pictures;
+    }
+
+    public void showtoolbar(String tittle, boolean upButton, View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(tittle);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }
